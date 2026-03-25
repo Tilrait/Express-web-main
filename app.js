22434f9d2ec902d2806af7453f23fbc9fa361682
Bokov Roman <romabokov191@gmail.com>
2026-03-25 12:27:51 +0300

    add admin panel

--- app.js
+++ app.js
@@ -1,25 +1,27 @@
-import methodOverride from "method-override";
-import express, { urlencoded } from "express";
-import { config } from "dotenv";
-import "./source/models/__loaddatabase.js";
-import router from "./source/router.js";
-import { error500Handler, mainErrorHandler } from "./source/error-handlers.js";
-import cookieParser from "cookie-parser";
+import methodOverride from 'method-override';
+import express, { urlencoded } from 'express';
+import { config } from 'dotenv';
+import './source/models/__loaddatabase.js';
+import router from './source/router.js';
+import { error500Handler, mainErrorHandler } from './source/error-handlers.js';
+import cookieParser from 'cookie-parser';
+import { adminRouter, rootPath } from './source/admin/admin.js';
 
 config();
 
 const port = process.env.PORT || 8000;
 const app = express();
 
-app.use(cookieParser())
-app.use(urlencoded({ extended: true}));
-app.use(methodOverride("_method"));
+app.use(cookieParser());
+app.use(urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
-app.locals.appTitle = process.env.APPTITLE || "Express";
-app.set("view engine", "ejs");
-app.set("views", "./source/templates");
+app.locals.appTitle = process.env.APPTITLE || 'Express';
+app.set('view engine', 'ejs');
+app.set('views', './source/templates');
 
-app.use("/", router);
+app.use('/', router);
+app.use(rootPath, adminRouter);
 
 app.use(mainErrorHandler, error500Handler);
 
---
+++ source/admin/admin.js
@@ -0,0 +1,147 @@
+import AdminJS from 'adminjs';
+import AdminJSExpress from '@adminjs/express';
+import * as AdminJSMongoose from '@adminjs/mongoose';
+import { config } from 'dotenv';
+import { Todo, User } from '../models/__loaddatabase.js';
+import { pbkdf2Promisified } from '../utility.js';
+import { deleteAllUserTodosModel } from '../models/todos.js';
+
+config();
+
+const rootPath = '/admin';
+
+AdminJS.registerAdapter({
+  Resource: AdminJSMongoose.Resource,
+  Database: AdminJSMongoose.Database,
+});
+
+const admin = new AdminJS({
+  resources: [
+    // объект ресурса для пользователя
+    {
+      resource: User,
+      options: {
+        properties: {
+          password: {
+            isVisible: {
+              list: false, // не показывать пароль в списке пользователей
+              filter: false, // не использовать пароль в фильтрах по пользователям
+              show: false, // Не показывать в подробной информации о пользователе
+              edit: true, // Показывать при создании
+            },
+          },
+          salt: {
+            isVisible: {
+              list: false, // не показывать соль в списке пользователей
+              filter: false, // не использовать соль в фильтрах по пользователям
+              show: false, // Не показывать в подробной информации о пользователе
+              edit: false, // не показывать при создании
+            },
+          },
+        },
+        Action: {
+          edit: { isAccessible: false },
+          new: {
+            before: async (request) => {
+              const rawPassword = request.payload.password;
+              if (typeof rawPassword !== 'string' || rawPassword.trim().length === 0) {
+                throw new Error('Пароль обязателен');
+              }
+              const salt = randomBytes(16);
+              const hash = await pbkdf2Promisified(req.body.password, salt, 100000, 32, 'sha256');
+
+              request.payload = {
+                ...request.payload,
+                password: hash,
+                salt: salt,
+              };
+              return request;
+            },
+          },
+          delete: {
+            before: async (request) => {
+              const userId = request.params.recordId;
+              if (userId) await deleteAllUserTodosModel(userId);
+              return request;
+            },
+          },
+        },
+      },
+    },
+    // ресурс для дел
+    {
+      resource: Todo,
+      options: {
+        properties: {
+          user: {
+            isVisible: {
+              list: true,
+              filter: true,
+              show: true,
+              edit: true,
+            },
+          },
+          title: {
+            isVisible: {
+              list: true,
+              filter: true,
+              show: true,
+              edit: true,
+            },
+          },
+          desc: {
+            isVisible: {
+              list: false,
+              filter: false,
+              show: true,
+              edit: true,
+            },
+          },
+          addendum: {
+            isVisible: {
+              list: false,
+              filter: false,
+              show: true,
+              edit: true,
+            },
+          },
+        },
+      },
+    },
+  ],
+  rootPath: rootPath,
+  branding: {
+    companyName: 'Админка megalast',
+    logo: false,
+  },
+});
+
+const DEFAULT_ADMIN = {
+  email: process.env.ADMIN_EMAIL || 'defaultAdmin@admin.com',
+  password: process.env.ADMIN_PASSWORD || 'admin',
+};
+
+async function authenticate(email, password) {
+  if (email === DEFAULT_ADMIN.email && password === DEFAULT_ADMIN.password)
+    return Promise.resolve(DEFAULT_ADMIN);
+  return false;
+}
+const adminRouter = AdminJSExpress.buildAuthenticatedRouter(
+  admin,
+  {
+    cookieName: process.env.ADMINJS_COOKIE_NAME || 'admingjs',
+    cookiePassword: process.env.ADMINJS_COOKIE_PASSWORD || 'adminjs-cookie-password',
+    authenticate: authenticate,
+  },
+  null,
+  {
+    secret: process.env.ADMINJS_SESSION_SECRET || 'adminjs-session-secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      httpOnly: true,
+    },
+  },
+);
+
+export { admin, adminRouter, rootPath };
